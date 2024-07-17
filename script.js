ee5333b3907aff84a82ceed496a051ee237a7e81
muhdarifrawi <muhdarifrawi@gmail.com>
2024-07-17 15:35:22 +0800

    updated

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ newDiv.setAttribute("id","container")
 document.body.appendChild(newDiv);
 
 let divContainer = document.getElementById("container");
-divContainer.style.height = "50vw";
+// divContainer.style.height = "10vw";
 divContainer.style.backgroundColor="Violet";
 
 function clickEvent(){
@@ -40,4 +40,48 @@ let arraySample = ["Arif", "Cat", "Dog"];
 for(i=0;i<arraySample.length;i++){
     sampleBox.innerHTML+=`<p>hello ${arraySample[i]}</p>`;
 }
+
+function validateUsername(){
+    let username = document.getElementById("username");
+    let usernameLength = username.value.length;
+    let usernameAlert = document.getElementById("username-alert");
+    usernameAlert.innerText = "";
+    // console.log(usernameLength);
+
+    if(usernameLength > 12){
+        // console.log("username too long");
+        usernameAlert.innerText += "Username too long";
+        usernameAlert.style.color = "red";
+    }
+    else{
+        // console.log("username okay");
+        usernameAlert.innerText += "Username okay";
+        usernameAlert.style.color = "green";
+    }
+}
+
+let formSubmitBtn = document.getElementById("form-submission");
+// formSubmitBtn.addEventListener("click", validateUsername);
+
+function validateUsernameByChange(){
+    // console.log(this.value)
+    let usernameLength = this.value.length;
+    let usernameAlert = document.getElementById("username-alert");
+    usernameAlert.innerText = "";
+    // console.log(usernameLength);
+
+    if(usernameLength > 12){
+        // console.log("username too long");
+        usernameAlert.innerText += "Username too long";
+        usernameAlert.style.color = "red";
+    }
+    else{
+        // console.log("username okay");
+        usernameAlert.innerText += "Username okay";
+        usernameAlert.style.color = "green";
+    }
+}
+
+let username = document.getElementById("username");
+username.addEventListener("change", validateUsernameByChange)
 
